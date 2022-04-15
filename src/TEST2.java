3f82a4c258e84f0abd9d3509746cc5eeabd822a4
hwan2272 <hwan2230@naver.com>
2022-04-15 20:51:30 +0900

    * Algorithms 백준문제풀이 추가 - Gold V [검문]

--- src/TEST2.java
+++ src/TEST2.java
@@ -8,44 +8,50 @@ import java.text.DecimalFormat;
 import java.util.*;
 public class TEST2 {
     static StringBuilder sb = new StringBuilder();
-    public static void main(String args[]) throws Exception {
-        //Scanner sc = new Scanner(System.in);
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.valueOf(br.readLine());
-        String[] arr = br.readLine().split(" ");
-        String[] resultArr = arr.clone();
 
-        //Stack<String> st = new Stack();
-        Stack<Nums> st = new Stack();
-        for(int i=0; i<n; i++) {
-            while(!st.isEmpty() && Integer.valueOf(arr[st.peek().idx]) < Integer.valueOf(arr[i])) {
-                resultArr[st.pop().idx] = arr[i];
+    public static int getGcd(int min, int max) {
+        int divided = min;
+        int gcd = 0;
+        while(divided > 0) {
+            if(max%min == 0) {
+                gcd = min;
+                return gcd;
             }
-            Nums nums = new Nums(i, arr[i]);
-            st.push(nums);
+            else {
+                divided = max%min;
+                max = min;
+                min = divided;
+            }
         }
-
-        while(!st.isEmpty()) {
-            resultArr[st.pop().idx] = "-1";
-        }
-
-        for(String x : resultArr) {
-            sb.append(x + " ");
-        }
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        return -1;
     }
 
-    public static class Nums {
-        int idx;
-        String data;
-
-        public Nums(int idx, String data) {
-            this.idx=idx;
-            this.data=data;
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.valueOf(br.readLine());
+        int[] arr = new int[n];
+        
+        //boolean[] bArr = new boolean[1000000001];
+        
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.valueOf(br.readLine());
         }
 
+        Arrays.sort(arr);
+        int gcd = arr[1]-arr[0];
+        for(int i=1; i<arr.length; i++) {
+            gcd = Math.min(gcd, getGcd(gcd,(arr[i]-arr[i-1])));
+        }
+
+        for(int i=2; i<=gcd; i++) {
+            if(gcd%i == 0) {
+                sb.append(i + " ");
+            }
+        }
+
+       bw.write(sb.toString());
+       bw.flush();
+       bw.close();
     }
 }
