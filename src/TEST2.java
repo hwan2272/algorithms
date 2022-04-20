78b5064156009803b9c9f6d584217d944f2f8efe
hwan2272 <hwan2230@naver.com>
2022-04-20 21:26:10 +0900

    * Algorithms 백준문제풀이 추가(그리디알고리즘)

--- src/TEST2.java
+++ src/TEST2.java
@@ -8,75 +8,30 @@ import java.text.DecimalFormat;
 import java.util.*;
 public class TEST2 {
     static StringBuilder sb = new StringBuilder();
-    static Queue<Position> q = new LinkedList();
-    static int n;
-    static int m;
-    static int[][] box;
-    static int[][] result;
-
-    static int[] nxArr = {-1, 0, 1, 0};
-    static int[] nyArr = {0, 1 ,0, -1};
-
-    public static void BFS() {
-        while(!q.isEmpty()) {
-            Position thisP = q.poll();
-            for(int i=0; i<4; i++) {
-                int nx = thisP.x + nxArr[i];
-                int ny = thisP.y + nyArr[i];
-                if(nx >= 0 && nx<m && ny >= 0 && ny<n &&  box[nx][ny] == 0) {
-                    box[nx][ny] = 1;
-                    q.offer(new Position(nx, ny));
-                    result[nx][ny] = result[thisP.x][thisP.y] + 1;
-                }
-            }
-
-        }
-    }
-
+    
     public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        box = new int[m][n];
-        result = new int[m][n];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        int n = Integer.valueOf(input.split(" ")[0]);
+        int goalMoney = Integer.valueOf(input.split(" ")[1]);
+        int[] moneyArr = new int[n];
+        int cnt = 0;
+        for(int i=0; i<n; i++) {
+            moneyArr[i] = Integer.valueOf(br.readLine());
+        }
+        Arrays.sort(moneyArr);
 
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                box[i][j] = sc.nextInt();
-                if(box[i][j] == 1) {
-                    q.offer(new Position(i, j));
-                }
+        for(int i=moneyArr.length-1; i>-1; i--) {
+            if(moneyArr[i] <= goalMoney && goalMoney > 0) {
+                cnt += goalMoney/moneyArr[i];
+                goalMoney = goalMoney%moneyArr[i];
             }
         }
-        BFS();
+        sb.append(cnt);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
 
-        boolean flag = true;
-        int days = Integer.MIN_VALUE;
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                if(box[i][j] == 0) {
-                    flag = false;
-                }
-            }
-        }
-        if(flag) {
-            for(int i=0; i<m; i++) {
-                for(int j=0; j<n; j++) {
-                    days = Math.max(days, result[i][j]);
-                }
-            }
-        }
-        else days = -1;
-        System.out.println(days);
-    }
-
-    public static class Position {
-        int x;
-        int y;
-
-        public Position(int x, int y) {
-            this.x=x;
-            this.y=y;
-        }
     }
 }
